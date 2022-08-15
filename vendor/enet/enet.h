2f63c23ff6c639e5a9de750d7c5f82e420a1666e
Jake <>
2022-08-15 00:37:01 -0700

    Minimize Windows' cancerous influence from enet.h

--- vendor/enet/enet.h
+++ vendor/enet/enet.h
@@ -66,6 +66,48 @@
  // =======================================================================//
 
 #if defined(_WIN32)
+// Jaker: added all these to minimize Windows.h leakage
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOCRYPT
+#define NOVIRTUALKEYCODES
+#define NOWINMESSAGES
+#define NOWINSTYLES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NORASTEROPS
+#define NOSYSCOMMANDS
+#define NOSHOWWINDOW
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOGDI
+#define NOKERNEL
+#define NOUSER
+#define NONLS
+#define NOMB
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMSG
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOWINOFFSETS
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
 #if defined(_MSC_VER) && defined(ENET_IMPLEMENTATION)
 #pragma warning (disable: 4267) // size_t to int conversion
 #pragma warning (disable: 4244) // 64bit to 32bit int
