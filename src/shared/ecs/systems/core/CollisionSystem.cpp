224d2b04267d7b2a7bfa17879d34126eb63ccde7
Jake <>
2022-09-20 02:15:43 -0700

    Small optimization and comments

--- src/shared/ecs/systems/core/CollisionSystem.cpp
+++ src/shared/ecs/systems/core/CollisionSystem.cpp
@@ -16,17 +16,22 @@ namespace shared::ecs
 
   void CollisionSystem::Update([[maybe_unused]] double dt)
   {
+    // This crappy O(n^2) algorithm will do for now.
     auto view = SceneRegistry()->view<ecs::Transform, ecs::Collider>();
     for (auto it0 = view.begin(); it0 != view.end();)
     {
       auto e0 = *it0;
       auto&& [transform0, collider0] = view.get(*it0);
-
+      
+      // Instead of doing a complicated OBB test, just expand the collider to fit the AABB of the rotated box.
       // https://stackoverflow.com/a/33867165
       glm::vec2 e0size = transform0.scale * .5f * collider0.scale;
-      glm::vec2 re0size;
-      re0size.x = e0size.x * std::abs(std::cos(transform0.rotation)) + e0size.y * std::abs(std::sin(transform0.rotation));
-      re0size.y = e0size.x * std::abs(std::sin(transform0.rotation)) + e0size.y * std::abs(std::cos(transform0.rotation));
+      glm::vec2 re0size = e0size;
+      if (transform0.rotation != 0)
+      {
+        re0size.x = e0size.x * std::abs(std::cos(transform0.rotation)) + e0size.y * std::abs(std::sin(transform0.rotation));
+        re0size.y = e0size.x * std::abs(std::sin(transform0.rotation)) + e0size.y * std::abs(std::cos(transform0.rotation));
+      }
       glm::vec2 e0max = transform0.translation + re0size;
       glm::vec2 e0min = transform0.translation - re0size;
 
@@ -35,9 +40,12 @@ namespace shared::ecs
         auto&& [transform1, collider1] = view.get(*it1);
 
         glm::vec2 e1size = transform1.scale * .5f * collider1.scale;
-        glm::vec2 re1size;
-        re1size.x = e1size.x * std::abs(std::cos(transform1.rotation)) + e1size.y * std::abs(std::sin(transform1.rotation));
-        re1size.y = e1size.x * std::abs(std::sin(transform1.rotation)) + e1size.y * std::abs(std::cos(transform1.rotation));
+        glm::vec2 re1size = e1size;
+        if (transform1.rotation != 0)
+        {
+          re1size.x = e1size.x * std::abs(std::cos(transform1.rotation)) + e1size.y * std::abs(std::sin(transform1.rotation));
+          re1size.y = e1size.x * std::abs(std::sin(transform1.rotation)) + e1size.y * std::abs(std::cos(transform1.rotation));
+        }
         glm::vec2 e1max = transform1.translation + re1size;
         glm::vec2 e1min = transform1.translation - re1size;
         
